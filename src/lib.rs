4d5d3feb84770cb0d94a5ed497797b9bf9836d3b
Patrick White <1275985+packysauce@users.noreply.github.com>
2022-06-15 14:02:50 -0400

    readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,4 @@
-//! I got tired of writing boilerplate to wire up functions to traits,
-//! so I made a helper
-//!
-//! Decorate an impl block with `#[dizpacho]` and you can then use `dizpacho` attributes
-//! on methods and associated functions to wire them up to whatever trait you like!
-//!
-//! ```rust
-//! struct TooLazyToType(String);
-//! struct OtherThing;
-//!
-//! #[dizpacho::dizpacho]
-//! impl TooLazyToType {
-//!     /// Just call my new() function for default!
-//!     #[dizpacho(Default::default)]
-//!     fn new() -> Self {
-//!         Self("howdy!".to_string())
-//!     }
-//! }
-//! assert_eq!(&TooLazyToType::default().0, "howdy!");
-//! ```
-//!
-//! ```rust
-//! struct TooLazyToType(String);
-//! struct OtherThing;
-//!
-//! #[dizpacho::dizpacho]
-//! impl TooLazyToType {
-//!     #[dizpacho(std::ops::Deref<Target = str>::deref)]
-//!     fn as_str(&self) -> &str {
-//!         &self.0
-//!     }
-//! }
-//! #[dizpacho::dizpacho]
-//! impl OtherThing {
-//!
-//!     /// You can even do generics!
-//!     #[dizpacho(From<Self>::from for TooLazyToType)]
-//!     fn from_other(thing: OtherThing) -> TooLazyToType {
-//!         TooLazyToType("I came from the other thing!".to_string())
-//!     }
-//! }
-//!
-//! assert!(TooLazyToType::from(OtherThing).0.ends_with("thing!"))
-//! ```
+#![doc = include_str!("../README.md")]
 
 use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
