5fdd2fec60f46906cd5047529757a26c4f13e058
Patrick White <1275985+packysauce@users.noreply.github.com>
2022-06-15 14:00:47 -0400

    std::iter example but dizpacho!

---
+++ tests/expand/iterator.expanded.rs
@@ -0,0 +1,57 @@
+struct MyCollection(Vec<i32>);
+#[automatically_derived]
+#[allow(unused_qualifications)]
+impl ::core::fmt::Debug for MyCollection {
+    fn fmt(&self, f: &mut ::core::fmt::Formatter) -> ::core::fmt::Result {
+        match *self {
+            MyCollection(ref __self_0_0) => {
+                let debug_trait_builder =
+                    &mut ::core::fmt::Formatter::debug_tuple(f, "MyCollection");
+                let _ = ::core::fmt::DebugTuple::field(debug_trait_builder, &&(*__self_0_0));
+                ::core::fmt::DebugTuple::finish(debug_trait_builder)
+            }
+        }
+    }
+}
+impl MyCollection {
+    fn new() -> MyCollection {
+        MyCollection(Vec::new())
+    }
+    fn add(&mut self, elem: i32) {
+        self.0.push(elem);
+    }
+    fn extend<T>(&mut self, iter: T) {
+        ::core::panicking::panic("not yet implemented")
+    }
+}
+impl std::iter::Extend<i32> for MyCollection {
+    fn extend<T>(&mut self, iter: T) {
+        <MyCollection>::extend(self, iter)
+    }
+}
+fn main() {
+    let mut c = MyCollection::new();
+    c.add(5);
+    c.add(6);
+    c.add(7);
+    c.extend(<[_]>::into_vec(box [1, 2, 3]));
+    match (&"MyCollection([5, 6, 7, 1, 2, 3])", &{
+        let res = ::alloc::fmt::format(::core::fmt::Arguments::new_v1(
+            &[""],
+            &[::core::fmt::ArgumentV1::new_debug(&c)],
+        ));
+        res
+    }) {
+        (left_val, right_val) => {
+            if !(*left_val == *right_val) {
+                let kind = ::core::panicking::AssertKind::Eq;
+                ::core::panicking::assert_failed(
+                    kind,
+                    &*left_val,
+                    &*right_val,
+                    ::core::option::Option::None,
+                );
+            }
+        }
+    };
+}
---
+++ tests/expand/iterator.rs
@@ -0,0 +1,32 @@
+#[derive(Debug)]
+struct MyCollection(Vec<i32>);
+
+#[dizpacho::dizpacho]
+impl MyCollection {
+    fn new() -> MyCollection {
+        MyCollection(Vec::new())
+    }
+
+    fn add(&mut self, elem: i32) {
+        self.0.push(elem);
+    }
+
+    #[dizpacho(std::iter::Extend<i32>::extend<T>)]
+    fn extend<T>(&mut self, iter: T) {
+        todo!()
+    }
+}
+
+fn main() {
+    let mut c = MyCollection::new();
+
+    c.add(5);
+    c.add(6);
+    c.add(7);
+
+    // let's extend our collection with three more numbers
+    c.extend(vec![1, 2, 3]);
+
+    // we've added these elements onto the end
+    assert_eq!("MyCollection([5, 6, 7, 1, 2, 3])", format!("{c:?}"));
+}
